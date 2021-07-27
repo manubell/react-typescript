b0e21cdc2f41df0d7804d8d9dddacd59e4a5f1da
manu <manu.bellemans@gmail.com>
2021-07-27 18:08:02 +0200

    basic types

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // let firstValue: string = "Manu";
+  // let firstValue: number = 1;
+  // let firstValue: boolean = true;
+  // let firstValue: number[] = [2, 3, 35];
+  let firstValue: Array<String> = ["2", "3", "35"];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          The value {} is of {typeof firstValue} type!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
