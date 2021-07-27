78e8159224e500a8585f353b1c1206078c49ee6d
manu <manu.bellemans@gmail.com>
2021-07-27 18:17:49 +0200

    typescript component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Message from "./Message";
 
 function App() {
-  // tuple
-  let aTuple: [string, number] = ["Manu", 1];
-  // enum
-  enum Codes {
-    first = 1,
-    second = 2,
-  }
-  //any
-  let firstname: any = "Manu";
-  // void
-  const warning = (): void => {
-    console.log("warning");
-  };
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The value {firstname} is of {typeof firstname} type!
-        </p>
+        <Message message={"This is a simple message"} />
       </header>
     </div>
   );
---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Message = (prop: { message: string }) => {
+  return <p>{prop.message}</p>;
+};
+
+export default Message;
